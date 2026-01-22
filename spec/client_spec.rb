faa52390818eee4edaa0e9c20e3ddd8d0e1afdbc
Alex Koch <kochalex@users.noreply.github.com>
2026-01-22 13:27:13 -0600

    expose student identifier field

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -698,7 +698,8 @@ RSpec.describe OneRoster::Client do
           provider: 'oneroster',
           email: student_1['email'],
           grades: student_1['grades'],
-          school_id: nil
+          school_id: nil,
+          identifier: student_1['identifier']
         )
       end
     end
